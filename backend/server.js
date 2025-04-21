766eb2aa89419c41eb6cc2ba3d4ef0c41b496dec
Akash Verma <akashvermastp7@gmail.com>
2025-04-21 15:47:25 +0530

    Initial commit 🚀

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import studentRoutes from './routes/studentRoutes.js';
+import adminRoutes from "./routes/adminRoutes.js";
+import teacherRoutes from "./routes/teacherRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+app.use(express.json());
+
+app.use('/api/student', studentRoutes);
+app.use('/api/teacher', teacherRoutes);
+app.use("/api/admin", adminRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected Successfully'))
+.catch(err => console.error('MongoDB Connection Error:', err));
+
+
+app.get('/', (req, res) => {
+  res.send('Student Registration API is Running');
+});
+
+
+const PORT = process.env.PORT || 4010;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
